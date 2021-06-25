a47a22ffb913153b16b3ef4be8c7357f46aa1e2a
Shohei Jinno <shohei.jinno@gmail.com>
2021-06-25 11:18:11 -0700

    resistor-color-duo: Simply use arithmetic operation instead of `parseInt()`.

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,10 +1,6 @@
 import { colorCode } from '../resistor-color/resistor-color';
 
 export const decodedValue = (colorDuo) => {
-    return parseInt(
-        colorDuo
-            .slice(0, 2)
-            .map((c) => colorCode(c))
-            .join('')
-    );
+    let [first, second] = colorDuo.slice(0, 2).map((c) => colorCode(c));
+    return 10 * first + second;
 };
