94d1b19bdda22bd1d4dda108fba2702bc0ecc905
Shohei Jinno <shohei.jinno@gmail.com>
2021-06-25 18:23:54 -0700

    gigasecond: Finish exercise.

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -1,8 +1,2 @@
-//
-// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const gigasecond = () => {
-  throw new Error('Remove this statement and implement this function');
-};
+export const gigasecond = (date) =>
+    new Date(date.getTime() + 1_000_000_000_000);
